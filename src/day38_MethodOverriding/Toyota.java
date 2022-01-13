7ba621252a361d15bf93c03566e7ebf07e234673
Chinara Orujova <china.orujova@gmail.com>
2022-01-13 10:55:45 -0500

    day 38: child class

--- src/day38_MethodOverriding/Toyota.java
+++ src/day38_MethodOverriding/Toyota.java
@@ -1,7 +1,13 @@
 package day38_MethodOverriding;
 
 public class Toyota extends Car{
-    public Toyota(String brand, String model, int year, double price, String color, int miles) {
-        super(brand, model, year, price, color, miles);
+    public Toyota( String model, int year, double price, String color, int miles) {
+        super("Toyota", model, year, price, color, miles);
     }
+
+    public void reliable(){
+        System.out.println(brand+" "+model+ " is reliable");
+    }
+
+
 }
