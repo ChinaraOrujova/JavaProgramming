b9b4543b11d4814102d8c8ace30ab92fb8521c98
Chinara Orujova <china.orujova@gmail.com>
2022-01-13 11:47:18 -0500

    day 38: child class

---
+++ src/day38_MethodOverriding/ParkingLot.java
@@ -0,0 +1,9 @@
+package day38_MethodOverriding;
+
+public class ParkingLot {
+    public static void main(String[] args) {
+        Toyota toyota1=new Toyota("Highlander",2016,45000,"gray",50000);
+        Tesla tesla1=new Tesla("Model S", 2022, 94990, "Red", 0);
+        BMW bmw1=new BMW("X6", 2022, 67350, "White", 0);
+    }
+}
