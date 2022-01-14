412247fd34553d72658af2b8fc0de76f9d3e4d70
Chinara Orujova <china.orujova@gmail.com>
2022-01-14 16:16:45 -0500

    day 39: Recap

---
+++ src/day39_Recap/Cydeo/Student.java
@@ -0,0 +1,33 @@
+package day39_Recap.Cydeo;
+
+public class Student extends Person{
+
+    private int studentId;
+    private String fieldOfStudy;
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFieldOfStudy() {
+        return fieldOfStudy;
+    }
+
+    public void setFieldOfStudy(String fieldOfStudy) {
+        this.fieldOfStudy = fieldOfStudy;
+    }
+
+    public void study(){
+        System.out.println(getName()+" is studing");
+    }
+
+    public Student(String name, int age, char gender, int studentId, String fieldOfStudy) {
+        super(name, age, gender);
+        setStudentId(studentId);
+        setFieldOfStudy(fieldOfStudy);
+    }
+}
