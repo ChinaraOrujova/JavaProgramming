103803a10298384644b23eb8dbed19931e3b8822
Chinara Orujova <china.orujova@gmail.com>
2022-01-17 11:27:14 -0500

    test

--- src/replit/Test.java
+++ src/replit/Test.java
@@ -1,26 +1,18 @@
 package replit;
-import day29_ArrayList.ReverseArrayVsArrayList;
+
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
-import java.util.concurrent.ScheduledExecutorService;
-
 
 public class Test {
     public static void main(String[] args) {
-        String [] cities ={"Boston","Houston","Austin","lincoln","Tusla"};
-        ArrayList<String> list =new ArrayList<>(Arrays.asList(cities));
-        int a= list.size();
 
-        for(String str:list){
-            String rev="";
-            for(int i=str.length()-1;i>=0;i--){
-                rev+=str.charAt(i);
-            }
-            list.set(--a,rev);
-        }
-        System.out.println(list);
+        ArrayList<String> list=new ArrayList<>(Arrays.asList("Apple","Grape","Lemon"));
+        int a=list.size();
+        int b=list.indexOf("Grape");
+        System.out.println(a+b);
+
+
 
     }
 }
